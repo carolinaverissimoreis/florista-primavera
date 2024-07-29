c829b78024d6f514e05b159c2402e22ba8a3a007
carolinavreis <me@carolinareis.net>
2024-07-29 23:18:53 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import './App.css'
+import logo from "./media/logo-horizontal.png"
+const { Header, Content, Footer } = Layout;
+
+const items = new Array(3).fill(null).map((_, index) => ({
+  key: String(index + 1),
+  label: `nav ${index + 1}`,
+}));
+
+const App: React.FC = () => {
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+
+  return (
+    <Layout className="florista-primavera">
+      <Header
+        style={{
+          position: 'sticky',
+          top: 0,
+          zIndex: 1,
+          width: '100%',
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
+        <div className="header-logo"><img alt="logo"src={logo}></img></div>
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['2']}
+          items={items}
+          style={{ flex: 1, minWidth: 0 }}
+        />
+      </Header>
+      <Content style={{ padding: '0 48px' }}>
+        <Breadcrumb style={{ margin: '16px 0' }}>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+        <div
+          style={{
+            padding: 24,
+            minHeight: 380,
+            background: colorBgContainer,
+            borderRadius: borderRadiusLG,
+          }}
+        >
+          Content
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>
+        Florista Primavera ©{new Date().getFullYear()} Created by Carolina Reis
+      </Footer>
+    </Layout>
+  );
+};
+
+export default App;
